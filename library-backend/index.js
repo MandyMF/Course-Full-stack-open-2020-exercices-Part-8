ab8fda1d0b3e8f03fe6bb3bd70b6e7104ef60590
MandyMF <mandy@fonoma.com>
2020-11-12 08:30:13 -0500

    Exercise 8.2

--- library-backend/index.js
+++ library-backend/index.js
@@ -84,16 +84,26 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    id: ID!
+    title: String!
+    author: String!
+    published: String!
+    genres: [String!]!
+  }  
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: ()=> books
   }
 }
 
